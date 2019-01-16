2460de4f1ac1f21810fba5fea066fdad112c6eb4
Rahmat Hidayat <kickyou44@gmail.com>
2019-01-16 19:49:08 +0700

    make Text stateless

--- src/components/atoms/Text.js
+++ src/components/atoms/Text.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-class Text extends React.Component {
-  render() {
-    return <p {...this.props} />
-  }
+// stateful component
+// class Text extends React.Component {
+//   render() {
+//     return <p {...this.props} />
+//   }
+// }
+
+// stateless component
+const Text = (props) => {
+  return <p {...props} />
 }
 
 export default Text
