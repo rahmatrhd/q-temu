a1e6fc63d03783cfd548665ebd6a9b47c9e3cf81
Rahmat Hidayat <kickyou44@gmail.com>
2019-01-16 20:38:22 +0700

    restyling Button

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 class Button extends React.Component {
   render() {
-    return <button {...this.props} />
+    return (
+      <button
+        {...this.props}
+        style={{
+          padding: '10px 30px',
+          fontWeight: 'bold',
+          textTransform: 'uppercase',
+        }}
+      />
+    )
   }
 }
 
