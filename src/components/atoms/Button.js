9238bcc8630cd760e2901f82b92b52684714bbaf
Rahmat Hidayat <kickyou44@gmail.com>
2019-01-23 20:40:36 +0700

    try styled emotion

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import { css } from 'emotion'
+
+const styles = {
+  button: {
+    padding: '10px 30px',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    ':hover': {
+      backgroundColor: 'rgba(0,0,0,0.2)',
+    },
+  },
+}
 
 class Button extends React.Component {
   render() {
     return (
       <button
         {...this.props}
-        style={{
-          padding: '10px 30px',
-          fontWeight: 'bold',
-          textTransform: 'uppercase',
-        }}
+        className={css(styles.button)}
       />
     )
   }
