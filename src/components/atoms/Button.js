52284845cb633a67efcf77b15782d8d22a3cd011
Rahmat Hidayat <kickyou44@gmail.com>
2019-01-23 20:57:37 +0700

    materialize Button component

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,26 +1,3 @@
-import React from 'react'
-import { css } from 'emotion'
-
-const styles = {
-  button: {
-    padding: '10px 30px',
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    ':hover': {
-      backgroundColor: 'rgba(0,0,0,0.2)',
-    },
-  },
-}
-
-class Button extends React.Component {
-  render() {
-    return (
-      <button
-        {...this.props}
-        className={css(styles.button)}
-      />
-    )
-  }
-}
+import Button from '@material-ui/core/Button'
 
 export default Button
