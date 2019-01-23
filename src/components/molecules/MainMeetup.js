25cb42e57d9ebdf579de772686a0f9c38c7a0d6a
Rahmat Hidayat <kickyou44@gmail.com>
2019-01-23 21:39:01 +0700

    gridify MainMeetup

--- src/components/molecules/MainMeetup.js
+++ src/components/molecules/MainMeetup.js
@@ -2,29 +2,41 @@ import React from 'react'
 import Text from '../atoms/Text'
 import Avatar from '../atoms/Avatar'
 import Button from '../atoms/Button'
+import Grid from '@material-ui/core/Grid'
+import withWidth from '@material-ui/core/withWidth'
 
-const MainMeetup = () => (
-  <div style={{ display: 'flex'}}>
-    <div style={{ marginRight: 30, }}>
-      <Avatar
-        style={{ width: 150, height: 150 }}
-        src="https://placehold.it/200x200"
-      />
-    </div>
-    <div style={{ flex: 1 }}>
-      <Text variant="title" gutterBottom>Hacktiv8 Meetup</Text>
-      <Text gutterBottom>
-        Location: Jakarta, Indonesia
-      </Text>
-      <Text gutterBottom>
-        Members: 1,024
-      </Text>
-      <Text gutterBottom>
-        Organizers: Adhy Wiranata
-      </Text>
-      <Button color="primary" variant="raised" size="large">Join Us</Button>
-    </div>
-  </div>
-)
+const MainMeetup = ({ width }) => {
+  const isMobileScreen = width === 'xs' || width === 'sm'
 
-export default MainMeetup
+  return (
+    <Grid container spacing={16} direction={isMobileScreen ? 'column' : 'row'} alignItems="center">
+      <Grid item>
+        <Avatar
+          style={{ width: 150, height: 150 }}
+          src="https://placehold.it/200x200"
+        />
+      </Grid>
+      <Grid item xs>
+        <Text variant="title" gutterBottom>Hacktiv8 Meetup</Text>
+        <Text gutterBottom>
+          Location: Jakarta, Indonesia
+        </Text>
+        <Text gutterBottom>
+          Members: 1,024
+        </Text>
+        <Text gutterBottom>
+          Organizers: Adhy Wiranata
+        </Text>
+        <Button color="primary" variant="raised" size="large">Join Us</Button>
+      </Grid>
+    </Grid>
+  )
+}
+
+// cosnt withWidth = () => {
+//   return (Component) => {}
+// }
+// const applyWidth = withWidth()
+// export default applyWidth(MainMeetup)
+
+export default withWidth()(MainMeetup)
