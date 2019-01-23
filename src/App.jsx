685d1c558a53db7becbdca0c9028fd6377bedecc
Rahmat Hidayat <kickyou44@gmail.com>
2019-01-23 22:01:05 +0700

    use MuiThemeProvider and theme config

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+
+import orange from '@material-ui/core/colors/orange'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: orange,
+  },
+})
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home />
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div>
+          <Home />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
