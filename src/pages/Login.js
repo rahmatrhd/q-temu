e026b2a469e6aad58b36448c12b6d56fbca83898
Rahmat Hidayat <kickyou44@gmail.com>
2019-01-28 20:14:50 +0700

    get queries

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,19 +2,24 @@ import React from 'react'
 import Text from '../components/atoms/Text'
 import TextField from '@material-ui/core/TextField'
 
-const Login = () => (
-  <React.Fragment>
-    <Text variant="title">
-      Login Page
-    </Text>
-    <TextField
-      placeholder="username"
-    />
-    <TextField
-      type="password"
-      placeholder="password"
-    />
-  </React.Fragment>
-)
+const Login = (props) => {
+  const queries = new URLSearchParams(props.location.search)
+
+  return (
+    <React.Fragment>
+      <Text variant="title">
+        Login Page
+      </Text>
+      <TextField
+        placeholder="username"
+      />
+      <TextField
+        type="password"
+        placeholder="password"
+      />
+      <button onClick={() => props.history.push('/profile')}>Login</button>
+    </React.Fragment>
+  )
+}
 
 export default Login
